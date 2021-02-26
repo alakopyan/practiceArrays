087425c0c741dc70692f0da9fa9a9ecdcc129316
alakopyan <79565761+alakopyan@users.noreply.github.com>
2021-02-26 16:32:18 +0300

    Update app.js

--- toDoApp/app.js
+++ toDoApp/app.js
@@ -11,30 +11,24 @@ function addTodo(todo) {
   let ul = document.querySelector("ul");
   let li = document.createElement("li");
 
-  const span = document.createElement("span")
+  const span = document.createElement("span");
   span.innerHTML = todo;
-  li.appendChild(span)
+  li.appendChild(span);
 
   const button = document.createElement("button");
   button.innerText = "Delete";
 
   button.addEventListener("click", deleteTodo);
   li.appendChild(button);
+
   li.classList.add("todo-list-item");
 
   li.addEventListener("click", () => {
-  
     if (li.style.textDecoration == "line-through")
       li.style.textDecoration = "none";
     else li.style.textDecoration = "line-through";
   });
   ul.appendChild(li);
-}
-
-document.querySelector("ul").addEventListener("click", checkDeleteBtn);
-
-function checkDeleteBtn(event) {
-  if (event.target.name == "deleteButton") deleteTodo(event);
 }
 
 function deleteTodo(event) {
